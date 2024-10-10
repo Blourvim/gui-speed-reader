5abf8a270cf2cd01be5c05837e057d1818c86979
blourvim <blourvim@gmail.com>
2024-10-10 19:24:55 +0300

    add simple logic to handle the input properly

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-mod view;
 mod model;
+mod view;
 
 use std::env;
+use std::io::{self, Write};
+
+use model::DisplayArray;
 
 fn main() {
+    // Get the input from the command line
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        panic!("usage: bin <text>");
+        panic!("Usage: <bin> <text>");
     }
-    let query = &args[1];
-    let words = query.split(" ");
+    let input = &args[1];
+    let words: Vec<&str> = input.split_whitespace().collect();
+    let mut display_array = DisplayArray::new();
 
+    for word in words {
+        display_array.print();
+        display_array.update(word);
+    }
+        io::stdout().flush().unwrap();
 }
 
--- src/model.rs
+++ src/model.rs
@@ -3,8 +3,7 @@ struct Rectangle {
     content: char,
 }
 
-struct DisplayArray {
-    // good len for starters
+pub struct DisplayArray {
     displays: [Rectangle; 26],
 }
 
@@ -18,29 +17,38 @@ impl Rectangle {
             self.content = new_char
         }
     }
+
+    pub fn display(&self) -> char {
+        self.content
+    }
 }
 
 impl DisplayArray {
     pub fn new() -> Self {
-        let mut boxes: [Rectangle; 26] = [Rectangle::new(' '); 26];
-
+        let boxes: [Rectangle; 26] = [Rectangle::new(' '); 26];
         Self { displays: boxes }
     }
 
     fn clear_display(&mut self) {
         self.displays.iter_mut().for_each(|f| f.change_char(' '));
     }
+
     pub fn update(&mut self, new_word: &str) -> &Self {
+        self.clear_display();
         let chars: Vec<char> = new_word.chars().collect();
         let word_len: i32 = chars.len().try_into().unwrap();
 
         let start_index = 13usize - ((word_len / 2) as f32).floor() as usize;
 
-        self.clear_display();
 
         for (i, c) in new_word.chars().take(26).enumerate() {
-            self.displays[start_index + i].change_char(c)
+            self.displays[start_index + i].change_char(c);
         }
         self
+    }
+
+    pub fn print(&self) {
+        let display_string: String = self.displays.iter().map(|f| f.display()).collect();
+        println!("{}", display_string);
     }
 }
