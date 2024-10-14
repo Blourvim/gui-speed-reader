5bb13d153b1d96bbcac3d523dc7ddf85b0e616b0
blourvim <blourvim@gmail.com>
2024-10-14 23:22:57 +0300

    spread each letter into different buttons

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod model;
 mod view;
 use std::time::Duration;
 
+use iced::mouse::Button;
 use iced::time;
+use iced::widget::{row, Row, Text};
 use iced::{widget::button, Element, Subscription, Theme};
 
 struct Reader {
@@ -16,6 +18,7 @@ enum Message {
     Increment,
 }
 
+// we do that here since the iced calls default when istantiating the Reader
 impl Default for Reader {
     fn default() -> Self {
         Self::new()
@@ -25,7 +28,6 @@ impl Reader {
     pub fn new() -> Self {
         let text = "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.";
         let words: Vec<String> = text.split_whitespace().map(String::from).collect();
-        println!("setting words");
         Self {
             words: words.clone(),
             index: 0usize,
@@ -34,21 +36,23 @@ impl Reader {
 
     pub fn update(&mut self, message: Message) {
         match message {
-            Message::Increment => {
-                println!("incrementing{:?}", &self.index);
-
-                self.index = self.index + 1
-            }
+            Message::Increment => self.index = self.index + 1,
         }
     }
 
     pub fn view(&self) -> Element<Message> {
         let word: &str = match self.words.len() {
-            0 => "no word",
+            0 => "There is no text",
             _ => &self.words[self.index],
         };
 
-        button(word).into()
+        let mut display_array = Row::new();
+
+        for ch in word.chars() {
+            display_array = display_array.push(button(Text::new(ch.to_string())));
+        }
+
+        display_array.into()
     }
 
     pub fn subscription(&self) -> Subscription<Message> {
